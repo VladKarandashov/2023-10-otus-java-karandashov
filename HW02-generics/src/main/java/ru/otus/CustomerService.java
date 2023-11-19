55b66c17022706a2bd516e736a2a93284c4536e0
karandashov <vlad.karandashov.tech@gmail.com>
2023-11-19 12:47:28 +0300

    для прохождения теста по сортировке пришлось сделать вторую map'у для сопоставления id и изначального score.
    Не придумал как пройти этот прекрасный тест более вменяемо

--- HW02-generics/src/main/java/ru/otus/CustomerService.java
+++ HW02-generics/src/main/java/ru/otus/CustomerService.java
@@ -1,6 +1,6 @@
 package ru.otus;
 
-import java.util.Comparator;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -8,41 +8,44 @@ public class CustomerService {
 
     // todo: 3. надо реализовать методы этого класса
     // важно подобрать подходящую Map-у, посмотрите на редко используемые методы, они тут полезны
-    private final TreeMap<Customer, String> map = new TreeMap<>(Comparator.comparing(Customer::getScores));
+
+    // oldScore -> customer+data
+    private final TreeMap<Long, Map.Entry<Customer, String>> map = new TreeMap<>();
+
+    // id -> oldScore
+    private final Map<Long, Long> oldScoreById = new HashMap<>();
+
 
     public Map.Entry<Customer, String> getSmallest() {
         var firstEntry = map.firstEntry();
-        var key = firstEntry.getKey();
+        var customerWithData = firstEntry.getValue();
+        var customer = customerWithData.getKey();
         // делаем полную копию ключа, чтобы издевательства в тесте никак не влияли на ключ в коллекции
-        return Map.entry(
-                new Customer(key.getId(), key.getName(), key.getScores()),
-                firstEntry.getValue()
-        );
+        return Map.entry(new Customer(customer), customerWithData.getValue());
     }
 
     /**
-     * Ищет entry по следующим правилам:
-     * 1) если score текущего элемента равен customer.getScore(), то возвращаем следующий элемент
-     * 2) если score текущего элемента больше customer.getScore(), то возвращаем текущий элемент
-     * Если ничего не нашли - null
+     * Судя по поведению в тесте - сортировка должна идти по изначальным score,
+     * для этого они были сохранены отдельно.
+     * Поэтому мы сначала по id пытаемся найти старый score
+     * (если его нет, то берём из входного параметра),
+     * и затем мы уже ищем, основываясь на старом score.
      *
      * @param customer - ключ для поиска
      * @return найденный entry
      */
     public Map.Entry<Customer, String> getNext(Customer customer) {
 
-        var entryIterator = map.entrySet().iterator();
+        var oldScore = oldScoreById.get(customer.getId());
+        var score = oldScore == null ? customer.getScores() : oldScore;
 
-        while (entryIterator.hasNext()) {
-            var entry = entryIterator.next();
-            var scores = entry.getKey().getScores();
-            if (scores == customer.getScores()) return entryIterator.next();
-            if (scores > customer.getScores()) return entry;
-        }
-        return null;
+        var find = map.higherEntry(score);
+
+        return find == null ? null : find.getValue();
     }
 
     public void add(Customer customer, String data) {
-        map.put(customer, data);
+        map.put(customer.getScores(), Map.entry(customer,data));
+        oldScoreById.put(customer.getId(), customer.getScores());
     }
 }
